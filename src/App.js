fa2a11996fe0947dcf82882730645e52cfa179b6
nterol <n.terol@24sevres.com>
2019-12-11 15:40:40 +0100

    🏗I made it work. Saving before new package.json

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
-import React, {useState} from 'react';
+import React, { Suspense, useState } from "react";
 
-import './App.css';
+import "./App.css";
+
+const MessageDisplayer = React.lazy(() =>
+  import("./components/MessageDisplayer"),
+);
 
 function App() {
-const [message, setMessage] = useState("");
-const [dadJoke, setDadJoke] = useState("");
-const [err, setError]= useState("");
+  const [message, setMessage] = useState("");
+  const [dadJoke, setDadJoke] = useState("");
+  const [err, setError] = useState("");
 
   const handleClick = async () => {
     try {
-      const res = await fetch("http://localhost:9000/.netlify/functions/hello");
-      const {message} = await res.json();
-      setMessage(message);
+      const res = await fetch("/.netlify/functions/hello");
 
+      const { message } = await res.json();
+
+      setMessage(message);
     } catch (err) {
       setError(err);
     }
-  }
+  };
 
   const handleDadJokes = async () => {
     try {
-      const res = await fetch("http://localhost:9000/.netlify/functions/dadJokes")
-      const {joke} = await res.json();
-      setDadJoke(joke)
+      const res = await fetch("/.netlify/functions/dadJokes");
+      const { joke } = await res.json();
+
+      setDadJoke(joke);
     } catch (err) {
-      setError(err)
+      setError(err);
     }
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={handleClick}>Aller go</button>
         <div>
-          {message && <span>{message}</span>}
-          {err && <span>there was an error : {err}</span>}
+          <Suspense fallback={<h1>Chargement du bonjour....</h1>}>
+            {(message || err) && <MessageDisplayer message={message || err} />}
+          </Suspense>
         </div>
-        <div><button onClick={handleDadJokes}>Get a DAD JOKE !</button></div>
-  <div>{dadJoke}</div>
-  <div>{err}</div>
+        <div>
+          <button onClick={handleDadJokes}>Get a DAD JOKE !</button>
+          <Suspense fallback={<h1>Chargement des blagues....</h1>}>
+            {(dadJoke || err) && <MessageDisplayer message={dadJoke || err} />}
+          </Suspense>
+        </div>
       </header>
     </div>
   );
--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,7 @@
 export function handler(event, context, callback) {
-    console.log('queryStringParameters', event.queryStringParameters)
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Hello, World!' }),
-    })
-  }
+  console.log("queryStringParameters", event.queryStringParameters);
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello, World!" }),
+  });
+}
